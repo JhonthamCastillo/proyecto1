ab19e90659f73ace33b1ea6df3427d71f2e3adf5
JhonathamCastillo <jhonathamcast@gmail.com>
2018-09-16 12:26:13 -0600

    He creado la ventana del programa

--- prueba/sistema/mi_prueba.py
+++ prueba/sistema/mi_prueba.py
@@ -1,3 +1,20 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
 
+class Ventana(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("hola a todos")
+        self.show()
+
+
+
+app = QApplication(sys.argv)
+
+v = Ventana()
+
+app.exec()
 
 print("hola mundo")
